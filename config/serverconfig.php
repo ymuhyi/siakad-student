7708c5b1009f4f4e64fe252a5bc313db6a78d21f
Muhyi <y.muhyi@gmail.com>
2024-10-26 18:05:00 +0700

    Polagro

--- config/serverconfig.php
+++ config/serverconfig.php
@@ -1,7 +1,7 @@
 <?php
 $database_host = "localhost";
-$database_username = "root";
-$database_password = "";
-$database_name = "dbsiakad";
+$database_username = "siakad";
+$database_password = "dakais";
+$database_name = "siakad";
 $db=new ee_database($database_host,$database_username,$database_password,$database_name);
 ?>
